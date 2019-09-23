05db1640604546c5b5f3d1140c99595fa7d6cb61
saivamshidobbali <u1266122@utah.edu>
2019-09-23 12:50:16 -0600

    Updated naming

--- HW2/Program__1__Ngrams__export/data/HW2_version1/init.py
+++
@@ -1,9 +0,0 @@
-def init(modified_data): 
-    DB = {}
-    for x in modified_data:
-        x= x.lower()
-        if x in DB:
-          DB[x] = DB[x]+1
-        else:
-          DB[x] = 1
-    return DB 
